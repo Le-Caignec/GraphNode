6a069bab400716ed1c652dbf28d8d8a612054839
Robin <roleciq@gmail.com>
2022-12-06 14:26:15 +0100

    improve script

--- GraphNode/deploy-subgraph.sh
+++ GraphNode/deploy-subgraph.sh
@@ -3,6 +3,6 @@ cd ../Subgraph
 
 # bellecour
 graph create bellecour --node http://127.0.0.1:8020
-graph deploy bellecour subgraph.yaml --node http://127.0.0.1:8020 --ipfs http://127.0.01:5001 --version-label 0.0.1
+graph deploy bellecour --node http://127.0.0.1:8020 --ipfs http://127.0.01:5001
 
 echo "browse bellecour subgraph at http://127.0.0.1:8000/subgraphs/name/bellecour/poco/graphql"
